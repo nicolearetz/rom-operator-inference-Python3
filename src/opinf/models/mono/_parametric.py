848fde1c8bf29e8142f6929e22c5935307ef79d6
naretz <nicole.aretz@t-online.de>
2025-10-22 16:19:06 -0500

    removed initial_guess from model classes

--- src/opinf/models/mono/_parametric.py
+++ src/opinf/models/mono/_parametric.py
@@ -273,11 +273,9 @@ class _ParametricModel(_OpInfModel):
             blocks.append(block.T)
         return np.hstack(blocks)
 
-    def _fit_solver(
-        self, parameters, states, lhs, inputs=None, initial_guess=None
-    ):
+    def _fit_solver(self, parameters, states, lhs, inputs=None):
         """Construct a solver for the operator inference least-squares
-        regression with regularization towards <initial_guess>"""
+        regression"""
         (
             parameters_,
             states_,
@@ -293,17 +291,6 @@ class _ParametricModel(_OpInfModel):
         # Set up non-intrusive learning.
         D = self._assemble_data_matrix(parameters_, states_, inputs_)
         R = np.hstack(lhs_)
-
-        if initial_guess is not None:
-            if initial_guess.shape != (R.shape[0], D.shape[1]):
-                raise RuntimeError(
-                    f"""In _NonparametricModel.refit:
-                                   initial_guess was passed of shape
-                                   {initial_guess.shape}, expected
-                                   {(R.shape[0], D.shape[1])}"""
-                )
-            R = R - (D @ initial_guess.T).T
-
         self.solver.fit(D, R)
         self.__s = len(parameters_)
 
@@ -324,7 +311,7 @@ class _ParametricModel(_OpInfModel):
                 op.set_entries(Ohat[:, index:endex])
             index = endex
 
-    def refit(self, initial_guess=None):
+    def refit(self):
         """Solve the Operator Inference regression using the data from the
         last :meth:`fit()` call, then extract the inferred operators.
 
@@ -334,9 +321,6 @@ class _ParametricModel(_OpInfModel):
         changing its ``regularizer`` attribute and calling this method solves
         the regression with the new regression value without re-factorizing the
         data matrix.
-
-        If an initial guess is provided, it will be added to the solution
-        of the regularized least squares problem.
         """
         if self._fully_intrusive:
             warnings.warn(
@@ -347,13 +331,10 @@ class _ParametricModel(_OpInfModel):
 
         # Execute non-intrusive learning.
         self._Ohat = self.solver.solve()
-        if initial_guess is not None:
-            self._Ohat += initial_guess
-
         self._extract_operators(Ohat=self._Ohat)
         return self
 
-    def fit(self, parameters, states, lhs, inputs=None, initial_guess=None):
+    def fit(self, parameters, states, lhs, inputs=None):
         r"""Learn the model operators from data.
 
         The operators are inferred by solving the regression problem
@@ -406,10 +387,6 @@ class _ParametricModel(_OpInfModel):
             corresponding to parameter value ``parameters[i]``; each column
             ``inputs[i][:, j]`` corresponds to the snapshot ``states[:, j]``.
             May be a two-dimensional array if :math:`m=1` (scalar input).
-        initial_guess : Initial guess for \Ohat as
-            array of shape (d(r, m), r). Used for Tikhonov
-            regularization. Assumed zero (classic Tikhonov regularization)
-            if not provided.
 
         Returns
         -------
@@ -422,10 +399,8 @@ class _ParametricModel(_OpInfModel):
             )
             return self
 
-        self._fit_solver(
-            parameters, states, lhs, inputs, initial_guess=initial_guess
-        )
-        return self.refit(initial_guess=initial_guess)
+        self._fit_solver(parameters, states, lhs, inputs)
+        return self.refit()
 
     # Parametric evaluation ---------------------------------------------------
     def evaluate(self, parameter):
@@ -553,7 +528,6 @@ class _ParametricDiscreteMixin:
         states,
         nextstates=None,
         inputs=None,
-        initial_guess=None,
     ):
         r"""Learn the model operators from data.
 
@@ -628,8 +602,6 @@ class _ParametricDiscreteMixin:
             corresponding to parameter value ``parameters[i]``; each column
             ``inputs[i][:, j]`` corresponds to the snapshot ``states[:, j]``.
             May be a two-dimensional array if :math:`m=1` (scalar input).
-        initial_guess: currently ignored,
-            only here for synergy with parent class
 
         Returns
         -------
@@ -763,7 +735,7 @@ class _ParametricContinuousMixin:
 
     _ModelClass = _FrozenContinuousModel
 
-    def fit(self, parameters, states, ddts, inputs=None, initial_guess=None):
+    def fit(self, parameters, states, ddts, inputs=None):
         r"""Learn the model operators from data.
 
         The operators are inferred by solving the regression problem
@@ -839,10 +811,6 @@ class _ParametricContinuousMixin:
             ``states[i][:, j]``.
             May be a two-dimensional array if :math:`m=1` (scalar input).
             Only required if one or more model operators depend on inputs.
-        initial_guess : Initial guess for \Ohat as
-            array of shape (d(r, m), r). Used for Tikhonov
-            regularization. Assumed zero (classic Tikhonov regularization)
-            if not provided.
 
         Returns
         -------
@@ -853,7 +821,6 @@ class _ParametricContinuousMixin:
             states,
             ddts,
             inputs=inputs,
-            initial_guess=initial_guess,
         )
 
     def rhs(self, t, parameter, state, input_func=None):
@@ -1181,14 +1148,9 @@ class _InterpModel(_ParametricModel):
             "_extract_operators() not used by this class"
         )
 
-    def _fit_solver(
-        self, parameters, states, lhs, inputs=None, initial_guess=None
-    ):
+    def _fit_solver(self, parameters, states, lhs, inputs=None):
         """Construct a solver for the operator inference least-squares
         regression.
-
-        initial_guess: currently does not get used, is here only
-        for synergy with the class structure
         """
         (
             parameters_,
@@ -1217,12 +1179,8 @@ class _InterpModel(_ParametricModel):
         self._submodels = nonparametric_models
         self._training_parameters = parameters_
 
-    def refit(self, initial_guess=None):
-        """Evaluate the least-squares solver and process the results.
-
-        initial_guess: currently not used, only here for synergy
-        with parent class
-        """
+    def refit(self):
+        """Evaluate the least-squares solver and process the results."""
         if self._submodels is None:
             raise RuntimeError("model solvers not set, call fit() first")
 
